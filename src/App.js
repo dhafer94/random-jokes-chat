e7fda7196390b2a33e463dfc4a63ed0bd02de10a
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-04-24 05:29:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [joke, setJoke] = useState();
+
+	const fetchJoke = async () => {
+		const res = await fetch('https://api.chucknorris.io/jokes/random');
+		const data = await res.json();
+		setJoke(data.value);
+	};
+	// console.log(joke);
+
+	return (
+		<div className='App'>
+			<div className='left-main-container'>
+				<div className='left-top-container'>
+					<div className='user-container'></div>
+					<div className='searchbar'></div>
+				</div>
+				<div className='left-bottom-container'>
+					<div className='chats-title'></div>
+					<div className='chats-contacts'></div>
+				</div>
+			</div>
+			<div className='right-main-container'>
+				<div className='right-top-container'></div>
+				<div className='right-middle-container'></div>
+				<div className='right-bottom-container'></div>
+			</div>
+			{/* <button onClick={fetchJoke}>Make a joke</button>
+			{joke} */}
+		</div>
+	);
 }
 
 export default App;
